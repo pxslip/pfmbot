69519454816d7e45fc268a07d396d86324ddbd8a
PX Slip <408277+pxslip@users.noreply.github.com>
2020-12-11 16:39:20 -0500

    Tested as working from local

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ dotenv.config();
 
 const client = new Client();
 
-client.on('ready', () => {});
+client.on('ready', () => {
+  console.log('pfmbot is listening');
+});
 
 client.on('messageReactionAdd', (reaction, user) => {
+  console.log('pfmbot hears you');
   if (reaction.emoji.name === '📌') {
     console.log(
       `Pinning message ${reaction.message.content} from user ${user.username}`
@@ -16,12 +19,17 @@ client.on('messageReactionAdd', (reaction, user) => {
 });
 
 client.on('messageReactionRemove', async (reaction, user) => {
+  console.log('You no like pfmbot...');
   const pushpins = await reaction.message.awaitReactions(
     (reaction, user) => reaction.emoji.name === '📌'
   );
   if (pushpins.size === 0) {
     reaction.message.unpin();
   }
+});
+
+client.on('error', (error) => {
+  console.error(error.message);
 });
 
 client.login(process.env.BOT_TOKEN);
